03b36a6fc2512c78f71ef04b10dba12babec6e25
Nirmal <nirmalkumarmcn@gmail.com>
2026-08-27 12:32:48 +0530

    made changes in navigation bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -261,7 +261,7 @@ function NavigationBar({ cartItemCount }) {
             </Menu>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1, minWidth: 0 }}>
               <Link to="/" className="logo-link">
-                FUSION ELECTRONICS
+                FUSION ELECTRONICS - UAT
               </Link>
             </Typography>
           </>
@@ -269,7 +269,7 @@ function NavigationBar({ cartItemCount }) {
           <>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', gap: 1, minWidth: 0 }}>
               <Link to="/" className="logo-link">
-                FUSION ELECTRONICS
+                FUSION ELECTRONICS - UAT
               </Link>
               <Typography component="span" variant="caption" sx={{ color: 'rgba(255,255,255,0.75)', whiteSpace: 'nowrap' }}>
                 Elevate Your Everyday Tech
